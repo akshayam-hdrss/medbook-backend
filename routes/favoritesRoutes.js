affb7fb8ef2baadddd975483f438d5914de9bd05
akshayam-hdrss <axolotronhdrss@gmail.com>
2025-12-04 19:04:29 +0530

    fav3

--- routes/favoritesRoutes.js
+++ routes/favoritesRoutes.js
@@ -2,10 +2,7 @@ const express = require("express");
 const router = express.Router();
 const favoritesController = require("../controllers/favoritesController");
 
-router.get(
-  "/favorites-details/:userId",
-  favoritesController.getFavoritesByUserdetails
-);
+router.get("/details/:userId", favoritesController.getFavoritesByUserdetails);
 router.post("/favorites", favoritesController.addFavorite);
 router.get("/favorites/:userId", favoritesController.getFavoritesByUser);
 router.put("/favorites/:id", favoritesController.updateFavorite);
